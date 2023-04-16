a49a2fb3bbf0284ed2a199f694bbd2461240ced3
Alfonso Jiménez <yo@alfonsojimenez.net>
2023-04-16 08:40:50 +0200

    Initial load

---
+++ src/main/java/Calculadora.java
@@ -0,0 +1,25 @@
+public class Calculadora {
+
+    double suma(double a, double b) {
+        return a + b;
+    }
+
+    double resta(double minuendo, double sustraendo) {
+        return minuendo - sustraendo;
+    }
+
+    double multiplica(double a, double b) {
+        return Math.abs(a * b);
+    }
+
+    double divide(double dividendo, double divisor) throws Exception {
+        if (divisor == 0) {
+            throw new Exception("El divisor es 0");
+        }
+        return dividendo / divisor;
+    }
+
+    double potencia(double base, double exponente) {
+        return Math.pow(base, exponente);
+    }
+}
---
+++ src/test/java/CalculadoraTest.java
@@ -0,0 +1,52 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraTest {
+
+    // Definimos el objeto para usarlo en todos los tests
+    static Calculadora miCalculadora;
+
+    @BeforeAll
+    public static void setUpClass(){
+        miCalculadora = new Calculadora();
+    }
+
+    @Test
+    @DisplayName("Multiplica dos números")
+    void testMultiplicar() {
+        assertEquals(6, miCalculadora.multiplica(2,3));
+        assertEquals(0, miCalculadora.multiplica(5,0));
+    }
+
+    @Test
+    @DisplayName("Divisiones enteras y reales")
+    void testDivisionesEnterasYReales() throws Exception {
+        assertEquals(2, miCalculadora.divide(4,2));
+        assertEquals(2.5, miCalculadora.divide(10,4));
+        assertEquals(4, miCalculadora.divide(10,2.5));
+        assertEquals(3.2, miCalculadora.divide(8,2.5));
+        assertEquals(0.5, miCalculadora.divide(2,4));
+    }
+
+    @Test
+    @DisplayName("Divisiones positivos y negativos")
+    void testDivisionesPositivosYNegativos() throws Exception {
+        assertTrue(miCalculadora.divide(4,2) > 0);
+        assertTrue(miCalculadora.divide(4,-2)< 0);
+        assertTrue(miCalculadora.divide(-4,2) < 0);
+        assertTrue(miCalculadora.divide(-4,-2) > 0);
+    }
+
+    @Test
+    @DisplayName("División por cero causa excepción")
+    void testDivisionPorCero() {
+        Exception thrown = assertThrows(Exception.class, () -> {
+            miCalculadora.divide(5,0);
+        });
+
+        assertEquals("El divisor es 0", thrown.getMessage());
+    }
+}
